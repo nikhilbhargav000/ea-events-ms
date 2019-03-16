e5f5ca0ccc78a0cf3500db42302763aa9cd7c421
Nikhil Bhargav <nikhil@Nikhils-MacBook-Air.local>
2019-03-16 14:48:12 +0530

    EventsApi Update New

---
+++ src/main/java/com/easyapper/eventsmicroservice/dao/helper/DaoHepler.java
@@ -0,0 +1,75 @@
+package com.easyapper.eventsmicroservice.dao.helper;
+
+import java.util.Date;
+import java.util.Map;
+import java.util.regex.Pattern;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.stereotype.Component;
+
+import com.easyapper.eventsmicroservice.exception.InvalidDateFormatException;
+import com.easyapper.eventsmicroservice.exception.InvalidTimeFormatException;
+import com.easyapper.eventsmicroservice.utility.EAConstants;
+import com.easyapper.eventsmicroservice.utility.EAUtil;
+import com.easyapper.eventsmicroservice.utility.EAValidator;
+
+@Component
+public class DaoHepler {
+	
+	@Autowired
+	EAValidator validator;
+
+	public void addSearchCriteriaForString(final String paramKey, final String dbEntityField, Query query, final Map<String, String> paramMap) throws InvalidTimeFormatException, InvalidDateFormatException {
+		if(paramMap.get(paramKey) != null) {
+			Pattern alikeCaseInsentitvePattern = Pattern.compile(Pattern.quote(paramMap.get(paramKey)) , Pattern.CASE_INSENSITIVE);
+			query.addCriteria(Criteria.where(dbEntityField).regex(alikeCaseInsentitvePattern));
+		}
+	}
+	
+	public void addSearchCriteriaForDate(final String paramFromDateKey, final String paramToDateKey, final String dbEntityField, Query query, final Map<String, String> paramMap) throws InvalidDateFormatException {
+		if(paramMap.get(paramFromDateKey) != null && 
+				paramMap.get(paramToDateKey) != null && 
+				validator.isValidDate(paramMap.get(paramFromDateKey)) &&
+				validator.isValidDate(paramMap.get(paramToDateKey)) ) {
+				query.addCriteria(Criteria.where(dbEntityField)
+						.gte(EAUtil.getDateFormatObj(paramMap.get(paramFromDateKey)))
+						.lte(EAUtil.getDateFormatObj(paramMap.get(paramToDateKey))));
+		}else if(paramMap.get(paramFromDateKey) != null && 
+				validator.isValidDate(paramMap.get(paramFromDateKey))) {
+			query.addCriteria(Criteria.where(dbEntityField)
+					.gte(EAUtil.getDateFormatObj(paramMap.get(paramFromDateKey))));
+		}else if(paramMap.get(paramToDateKey) != null
+				&& validator.isValidDate(paramMap.get(paramToDateKey))) {
+			query.addCriteria(Criteria.where(dbEntityField)
+					.lte(EAUtil.getDateFormatObj(paramMap.get(paramToDateKey))));
+		}
+	}
+	
+	public void addSearchCriteriaForTime(final String paramFromTimeKey, final String paramToTimeKey, final String dbEntityField, Query query, final Map<String, String> paramMap) throws InvalidDateFormatException {
+		if(paramMap.get(paramFromTimeKey) != null && 
+				paramMap.get(paramToTimeKey) != null && 
+				validator.isValidTime(paramMap.get(paramFromTimeKey)) &&
+				validator.isValidTime(paramMap.get(paramToTimeKey)) ) {
+				query.addCriteria(Criteria.where(dbEntityField)
+						.gte(EAUtil.getTimeFormatObj(paramMap.get(paramFromTimeKey)))
+						.lte(EAUtil.getTimeFormatObj(paramMap.get(paramToTimeKey))));
+		}else if(paramMap.get(paramFromTimeKey) != null && 
+				validator.isValidTime(paramMap.get(paramFromTimeKey))) {
+			query.addCriteria(Criteria.where(dbEntityField)
+					.gte(EAUtil.getTimeFormatObj(paramMap.get(paramFromTimeKey))));
+		}else if(paramMap.get(paramToTimeKey) != null
+				&& validator.isValidTime(paramMap.get(paramToTimeKey))) {
+			query.addCriteria(Criteria.where(dbEntityField)
+					.lte(EAUtil.getTimeFormatObj(paramMap.get(paramToTimeKey))));
+		}
+	}
+	
+	public void setPaginationInQuery(Query query, int page, int size, long skip){
+		long totalskip = ((page - 1) * size) + skip;
+		query.skip(totalskip);
+		query.limit(size);
+	}
+	
+}
---
+++ src/main/java/com/easyapper/eventsmicroservice/translator/EventsTranslator.java
@@ -0,0 +1,111 @@
+package com.easyapper.eventsmicroservice.translator;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+
+import com.easyapper.eventsmicroservice.entity.AddressSubEntity;
+import com.easyapper.eventsmicroservice.entity.EventBookingSubEntity;
+import com.easyapper.eventsmicroservice.entity.LocationSubEntity;
+import com.easyapper.eventsmicroservice.entity.PostedEventEntity;
+import com.easyapper.eventsmicroservice.entity.SubscribedEventEntity;
+import com.easyapper.eventsmicroservice.model.AddressDto;
+import com.easyapper.eventsmicroservice.model.EventBookingDto;
+import com.easyapper.eventsmicroservice.model.EventDto;
+import com.easyapper.eventsmicroservice.model.LocationDto;
+import com.easyapper.eventsmicroservice.utility.EAUtil;
+
+@Component
+public class EventsTranslator {
+	
+	public static SubscribedEventEntity getSubcribedEventEntity(EventDto eventDto) {
+		SubscribedEventEntity subscribedEventEntity = new SubscribedEventEntity(
+				eventDto.get_id(), eventDto.getUser_id(), eventDto.getPosted_event_id() 
+				, eventDto.getEvent_type(),
+				EAUtil.getDateFormatObj( eventDto.getEvent_start_date()), 
+				EAUtil.getDateFormatObj( eventDto.getEvent_last_date()),
+				EAUtil.getTimeFormatObj(eventDto.getEvent_start_time()),
+				EAUtil.getTimeFormatObj(eventDto.getEvent_end_time()) );
+		return subscribedEventEntity;
+	}
+	
+	public static EventDto getEventDto(SubscribedEventEntity subcEventEntity) {
+		EventDto eventDto = new EventDto(subcEventEntity.get_id(), subcEventEntity.getUser_id(),
+				subcEventEntity.getEvent_type(),
+				EAUtil.getDateFormatStr(subcEventEntity.getEvent_start_date()),
+				EAUtil.getDateFormatStr(subcEventEntity.getEvent_last_date()),
+				subcEventEntity.getPost_event_id(), 
+				EAUtil.getTimeFormatStr(subcEventEntity.getEvent_start_time()),
+				EAUtil.getTimeFormatStr(subcEventEntity.getEvent_end_time()));
+		return eventDto;
+	}
+	
+	public static EventDto getEventDto(PostedEventEntity eventEntity) {
+		LocationSubEntity locationEntity = eventEntity.getEvent_location();
+		AddressSubEntity addressEntity = locationEntity.getAddress();
+		EventBookingSubEntity bookingEntity = eventEntity.getEvent_booking();
+		
+		AddressDto addressDto = new AddressDto(addressEntity.getId(), addressEntity.getCity(),
+				addressEntity.getStreet(), addressEntity.getPin());
+		LocationDto locationDto = new LocationDto(locationEntity.getLongitude(),
+				locationEntity.getLatitude(), addressDto);
+		EventBookingDto bookingDto = new EventBookingDto(bookingEntity.getUrl(),
+				bookingEntity.getInquiry_url());
+		String startDate = EAUtil.getDateFormatStr(eventEntity.getEvent_start_date());
+		String lastDate = EAUtil.getDateFormatStr(eventEntity.getEvent_last_date());
+		EventDto eventDto = new EventDto(eventEntity.get_id(), eventEntity.getUser_id(),
+				eventEntity.getEvent_type(),
+				eventEntity.getEvent_category(), eventEntity.getEvent_subcategory(),
+				locationDto, eventEntity.getOrganizer_email(), eventEntity.getEvent_name(),
+				eventEntity.getEvent_description(), eventEntity.getEvent_image_url(),
+				startDate, lastDate,
+				eventEntity.getEvent_min_age(), eventEntity.getEvent_max_age(),
+				eventEntity.getEvent_price(), bookingDto, 
+				eventEntity.getOriginal_event());
+				
+		return eventDto;
+	}
+	
+	public static PostedEventEntity getPostedEventEntity(EventDto evenDto) {
+		LocationDto locationDto = evenDto.getEvent_location();
+		AddressDto addressDto = locationDto.getAddress();
+		EventBookingDto bookingDto = evenDto.getEvent_booking();
+		AddressSubEntity addressSubEntity = new AddressSubEntity(addressDto.getId(),
+				addressDto.getCity(), addressDto.getStreet(), addressDto.getPin());
+		LocationSubEntity locationSubEntity = new LocationSubEntity(
+				locationDto.getLongitude(), locationDto.getLatitude(), addressSubEntity);
+		EventBookingSubEntity bookingEntity = new EventBookingSubEntity(bookingDto.getUrl(),
+				bookingDto.getInquiry_url());
+		Date startDateObj = EAUtil.getDateFormatObj(evenDto.getEvent_start_date());
+		Date lastDateObj = EAUtil.getDateFormatObj(evenDto.getEvent_last_date());
+		PostedEventEntity eventEntity = new PostedEventEntity(evenDto.get_id(),
+				evenDto.getUser_id(), evenDto.getEvent_type(),
+				evenDto.getEvent_category(), evenDto.getEvent_subcategory(),
+				locationSubEntity, evenDto.getOrganizer_email(), 
+				evenDto.getEvent_name(), evenDto.getEvent_description(),
+				evenDto.getEvent_image_url(), startDateObj,
+				lastDateObj, evenDto.getEvent_min_age(),
+				evenDto.getEvent_max_age(), evenDto.getEvent_price(),
+				bookingEntity, evenDto.getOriginal_event());
+		return eventEntity;
+	}
+	
+	public List<EventDto> getPostedEventDtoList(List<PostedEventEntity> eventEntityList){
+		List<EventDto> eventDtoList = new ArrayList<>();
+		for(PostedEventEntity eventEntity : eventEntityList) {
+			eventDtoList.add(this.getEventDto(eventEntity));
+		}
+		return eventDtoList;
+	}
+	
+	public List<EventDto> getSubscribedEventDtoList(List<SubscribedEventEntity> subcEventEntityList){
+		List<EventDto> eventDtoList = new ArrayList<>();
+		for(SubscribedEventEntity subcEventEntity : subcEventEntityList) {
+			eventDtoList.add(this.getEventDto(subcEventEntity));
+		}
+		return eventDtoList;
+	}
+
+}
